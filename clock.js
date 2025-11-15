259775122d27ae614f31965d1e2f233d2e674014
Sneha Singh <snehasingh50246@gmail.com>
2025-11-15 15:11:24 +0530

    new features added

--- clock.js
+++ clock.js
@@ -5,13 +5,28 @@ const day = document.getElementById("day");
 const midday = document.getElementById("midday");
 const format12 = document.getElementById("format12");
 const format24 = document.getElementById("format24");
+const bgInput = document.getElementById("bg-input");
+const bgBtn = document.getElementById("bg-btn");
+const resetBgBtn = document.getElementById("reset-bg-btn");
+const dateVar = document.getElementById("date");
+//default background
+const defaultbg = "url('./src/bgImages.jpg')";
+//default format choice
 let is24Hour = false;
 const clock = setInterval(function time() {
   let dateToday = new Date();
+  //adding date
+  let date = dateToday.getDate().toString().padStart(2, "0");
+  let month = (dateToday.getMonth() + 1).toString().padStart(2, "0");
+  let year = dateToday.getFullYear();
+  dateVar.innerText = `${date}/${month}/${year}`;
+  //getting hour , minutes, seconds
   let hr = dateToday.getHours();
   let min = dateToday.getMinutes();
   let sec = dateToday.getSeconds();
+
   let middayVal = "";
+  //display day
   let days = [
     "Sunday",
     "Monday",
@@ -48,13 +63,45 @@ const clock = setInterval(function time() {
   seconds.textContent = sec;
   midday.textContent = is24Hour ? "" : middayVal;
 }, 1000);
+// format switcher
 format12.addEventListener("click", () => {
   is24Hour = false;
-  format12.style.color = "#00bfff";
+  format12.style.color = "#060e41ff";
   format24.style.color = "#fff";
 });
 format24.addEventListener("click", () => {
   is24Hour = true;
-  format24.style.color = "#00bfff";
+  format24.style.color = "#060e41ff";
   format12.style.color = "#fff";
+});
+//open file picker
+bgBtn.addEventListener("click", () => {
+  bgInput.click();
+});
+// when user selects image
+bgInput.addEventListener("change", () => {
+  const file = bgInput.files[0];
+  if (!file) {
+    return;
+  }
+  //assigning url to the selected picture
+  const imageURL = URL.createObjectURL(file);
+  //set body background
+  document.body.style.backgroundImage = `url(${imageURL})`;
+  //save to local storage
+  localStorage.setItem("bodyBackground", imageURL);
+});
+//restore background on refresh
+window.addEventListener("load", () => {
+  const savedBG = localStorage.getItem("bodyBackground");
+  if (savedBG) {
+    document.body.style.backgroundImage = `url(${savedBG})`;
+  } else {
+    document.body.style.backgroundImage = defaultbg;
+  }
+});
+//reset background
+resetBgBtn.addEventListener("click", () => {
+  localStorage.removeItem("bodyBackground");
+  document.body.style.backgroundImage = defaultbg;
 });
